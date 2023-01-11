0e5e87ee75528f7194be7a709d9ed2d7a8cb4df1
ksr0818 <ksr940818@gmail.com>
2023-01-11 20:49:26 +0900

    첫번째 commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main implements ExampleInterface {
+    @Override
+    public void example() {
+
     }
 }
